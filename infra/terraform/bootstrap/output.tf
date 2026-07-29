be1a704f52af8c1e697b89c0135710c9c254606b
Ho Hoang Linh (MS/EDS32-XC) <Linh.HoHoang@vn.bosch.com>
2026-07-30 05:57:14 +0700

    S3 state lock file

--- infra/terraform/bootstrap/output.tf
+++ infra/terraform/bootstrap/output.tf
@@ -3,11 +3,6 @@ output "state_bucket_name" {
   value       = aws_s3_bucket.terraform_state.id
 }
 
-output "lock_table_name" {
-  description = "DynamoDB table for Terraform state locking"
-  value       = aws_dynamodb_table.terraform_lock.name
-}
-
 output "state_bucket_arn" {
   description = "ARN of the state bucket"
   value       = aws_s3_bucket.terraform_state.arn
