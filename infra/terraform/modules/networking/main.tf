9691b0ee21ce91d6f17fd38ec652df0640d83d47
Linh Hoang <linhhn508@gmail.com>
2026-07-31 03:02:39 +0700

    [infra:vpc] Enable dns_support and dns_hostname

--- infra/terraform/modules/networking/main.tf
+++ infra/terraform/modules/networking/main.tf
@@ -4,7 +4,8 @@ data "aws_availability_zones" "available" {
 
 resource "aws_vpc" "main" {
   cidr_block       = var.vpc_cidr
-  instance_tenancy = "default"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = "${var.project_name}-vpc"
